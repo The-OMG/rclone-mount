7b5fe91339c9c806a00af275ebd545a75a289ceb
OMG <omg@orionsbelt.space>
2019-03-09 23:38:17 -0900

    code consolidation, formatting, new features
    - removed old code.
    - When mounting, the script will restart automatically if it doesn't exit with 0.
    - removed repetitive code
    - when running for the first time, the rclone mount will fail and try to create the $MPOINT, unmount the $MPOINT, print a error message, sleep 2, and then start again until it is successful.

--- Linux/rclone_mount.sh
+++ Linux/rclone_mount.sh
@@ -21,64 +21,32 @@
 ###  Install rclone       : "brew install rclone"
 
 _Main() {
-  # Global Vars:
-  # Path to rclone. In most cases, it will be in your $PATH so you shouldnt need to change this.
-  RCLONE="rclone"
-  # Do not change this option. I use the advanced backend options for mounting.
-  REMOTE=":drive:"
-  # Your Teamdrive ID. This can be found in the url at the root of your Teamdrive.
-  TD='0ANizVBO00Ao1Uk9PVA'
-  # Full path to the service account token for authentication of the Teamdrive.
-  SA="/home6/omg/.config/rclone/tokens/Owncloud-fb9ead49f333.json"
-  # Path to your mount folder.
-  MPOINT="$HOME/cloud/rom/"
-#  LOGFILE="$HOME/logs/rclone-rom.log"
-#  OPERATIONLOG="$HOME/logs/RcloneMountOperations.log"
+    # Global Vars:
+    RCLONE="$HOME/bin/rclone" # Path to rclone. In most cases, it will be in your $PATH so you shouldnt need to change this.
+    REMOTE=":drive:/TV Shows" # Do not change this option. I use the advanced backend options for mounting.
+    TD="0AEFojjZ0gu-9Uk9PVA"   # Your Teamdrive ID. This can be found in the url at the root of your Teamdrive.
+    SA="$HOME/.config/rclone/tokens/Owncloud-5ef5555144cb.json"   # Full path to the service account token for authentication of the Teamdrive.
+    MPOINT="$HOME/cloud/TV Shows/"   # Path to your mount folder.
 
-  ##############################################################################
-  # Functions:
-  _Setup() {
-    _MountCheck() { ## CHECK IF MOUNT ALREADY EXIST AND MOUNT IF NOT
-      if mountpoint -q "$MPOINT"; then
-        echo "$MPOINT already mounted" | tee -a "$OPERATIONLOG"
-      else
-        echo "Mounting $MPOINT" | tee -a "$OPERATIONLOG"
-      fi
-    }
-
- #   _LogFolder() { # Create log folder location if necessary.
- #     if [ ! -d "$HOME/logs" ]; then
- #       echo "Creating Mount Location" | tee -a "$OPERATIONLOG"
- #       mkdir -p "$HOME/logs"
- #     fi
- #   }
-
+    ##############################################################################
+    # Functions:
     _MountPoint() { # Create mount location if necessary.
-      echo "Creating Mount Location" | tee -a "$OPERATIONLOG"
-      if [ ! -d "$MPOINT" ]; then
-        mkdir -p "$MPOINT"
-      fi
+        echo "Creating Mount Location"
+        if [ ! -d "$MPOINT" ]; then
+            mkdir -p "$MPOINT"
+        fi
     }
 
-#    _TmpCache() { # Create temporary cache location if necessary.
-#      if [ ! -d "$TMPCACHE" ]; then
-#        echo "Creating Mount Location" | tee -a "$OPERATIONLOG"
-#        mkdir -p "$TMPCACHE"
-#      fi
-#    }
-    _MountCheck && _MountPoint && #_LogFolder && _TmpCache
-  }
+    _unmount() { # Unmount a mounted remote.
+        echo "Unmounting $REMOTE from $MPOINT"
+        fusermount -uz "$MPOINT"
+    }
 
-  _unmount() { # Unmount a mounted remote.
-    echo "Unmounting $REMOTE from $MPOINT" | tee -a "$OPERATIONLOG"
-    fusermount -uz "$MPOINT" | tee -a "$LOGFILE"
-  }
+    _install() { # Installs the latest rclone via linuxbrew.
+        brew install rclone
+    }
+    ##############################################################################
 
-  _install() { # Installs the latest rclone via linuxbrew.
-    brew install rclone | tee -a "$OPERATIONLOG"
-  }
-  ##############################################################################
-  _RcloneMount() {
     # Calculate best chunksize for transfer speed.
     AvailableRam=$(free --giga -w | grep Mem | awk '{print $8}')
     case "$AvailableRam" in
@@ -91,151 +59,103 @@ _Main() {
     [0-1]) driveChunkSize="8M" ;;
     esac
 
-    local TMPCACHE="$HOME/.config/rclone/tmp_upload_cache"
-    local rcloneARGS=(
-      "--allow-other"
-#      "--allow-root"
-#      "--contimeout=5s"
-#      "--daemon"
-      "--drive-chunk-size=$driveChunkSize"
-#      "--log-file=$LOGFILE"
-#      "--log-level=INFO"
-      # "--max-read-ahead=1M"
-#      "--rc"
-#      "--read-only"
-#      "--size-only"
-#      "--stats=10s"
-      "--tpslimit=10"
-      "--tpslimit-burst=10"
-#      "--umask=777"
-#      "--fuse-flag=sync_read"
-#      "--timeout=5s"
-      "-P"
-      "-vv"
+    rcloneARGS=(
+        "--allow-other"
+        "--drive-chunk-size=$driveChunkSize"
+        "--tpslimit=10"
+        "--tpslimit-burst=10"
+        "-P"
+        "-vvv"
     )
 
-    _purge() { # Purge Rclone Cache while mounted or unmounted.
-      if mountpoint -q "$MPOINT"; then
-        echo "Purging cache" | tee -a "$OPERATIONLOG"
-        killall -SIGHUP rclone
-      else
-        echo "Purging cache" | tee -a "$OPERATIONLOG"
-        _RcloneCacheMount --cache-db-purge
-        sleep 10
-        _unmount
-        sleep 5
-        echo "Purge Successful"
-        _PlexGuideRcache --cache-db-purge
-        sleep 10
-        _unmount
-        sleep 5
-        echo "Purge Successful"
-
-      fi
-    }
-
     _RcloneCacheMount() { # Expects to used with the "Cache" remote.
-      local rclonecacheARGS=(
-#        "--attr-timeout=1s"
-        "--buffer-size=0M"
-        "--cache-chunk-clean-interval=15m"
-#        "--cache-chunk-path=$HOME/.cache/rclone/zendrive/cache-backend"
-        '--cache-chunk-size=5M'
-#        "--cache-db-path=$HOME/.cache/rclone/zendrive/cache-backend"
-        "--cache-db-purge"
-#        "--cache-dir=$HOME/.cache/rclone/zendrive"
-#        "--cache-info-age=168m"
-        "--cache-read-retries=10"
-        "--cache-tmp-upload-path=$TMPCACHE"
-        "--cache-tmp-wait-time=15m"
-        '--cache-chunk-total-size=100G'
-        '--cache-workers=8'
-        "--cache-writes"
-        '--dir-cache-time=15m'
-        '--drive-scope=drive'
-        "--drive-service-account-file=$SA"
-        "--drive-team-drive=$TD"
-#        "--poll-interval=40s"
-#        "--vfs-cache-max-age=675h"
-        "--vfs-cache-mode=writes"
-#        "--vfs-read-chunk-size-limit=1G"
-#        "--vfs-read-chunk-size=32M"
-        #    "--vfs-cache-max-age=11h"
-        #    "--vfs-cache-mode=writes"
-        #    "--vfs-cache-poll-interval=1m"
-        "--write-back-cache"
-      )
-      _unmount
-      "$RCLONE" mount "$REMOTE" "$MPOINT" "${rcloneARGS[@]}" "${rclonecacheARGS[@]}" "$@" &&
-        echo "Mount Successful" || echo "Mount Failed"
+        rclonecacheARGS=(
+            "--buffer-size=0M"
+            "--cache-chunk-clean-interval=15m"
+            "--cache-chunk-size=5M"
+            "--cache-db-purge"
+            "--cache-read-retries=10"
+            "--cache-tmp-upload-path=$TMPCACHE"
+            "--cache-tmp-wait-time=15m"
+            "--cache-chunk-total-size=100G"
+            "--cache-workers=8"
+            "--cache-writes"
+            "--dir-cache-time=15m"
+            "--drive-scope=drive"
+            "--drive-service-account-file=$SA"
+            "--drive-team-drive=$TD"
+            "--vfs-cache-mode=writes"
+            "--write-back-cache"
+        )
+        until "$RCLONE" mount "$REMOTE" "$MPOINT" "${rcloneARGS[@]}" "${rclonecacheARGS[@]}" "$@"; do
+            _MountPoint
+            _unmount
+            echo "rclone mount for $MPOINT crashed with exit code $?.  Respawning.."
+            sleep 2
+        done
     }
- 
+
     _NormalMount() { # Mount Rclone without any caching.
-      "$RCLONE" mount "$REMOTE" "$MPOINT" "${rcloneARGS[@]}" &&
-        echo "Mount Successful" || echo "Mount Failed" &
+        until "$RCLONE" mount "$REMOTE" "$MPOINT" "${rcloneARGS[@]}"; do
+            _MountPoint
+            _unmount
+            echo "rclone mount for $MPOINT crashed with exit code $?.  Respawning.."
+            sleep 2
+        done
+    }
+
+    _purge() { # Purge Rclone Cache while mounted or unmounted.
+        if mountpoint -q "$MPOINT"; then
+            echo "Purging cache"
+            killall -SIGHUP rclone
+        else
+            echo "Purging cache"
+            _RcloneCacheMount --cache-db-purge
+            sleep 10
+            _unmount
+            sleep 5
+            echo "Purge Successful"
+        fi
+    }
+
+    ##############################################################################
+
+    _hlp() {
+        $ECHO ""
+        $ECHO "Usage: rclone_mount.sh <option(s)>"
+        $ECHO ""
+        $ECHO "Available options:"
+        $ECHO "-c, --cache    : Mount rclone remote with cacheing parameters"
+        $ECHO "-h, --help     : This help page."
+        $ECHO "-i, --install	: Installs rclone via linuxbrew"
+        $ECHO "-m, --mount    : Mount your rclone remote"
+        $ECHO "-p, --purge    : Will purge your rclone cache and mount."
+        $ECHO ""
     }
 
     while [ ! $# -eq 0 ]; do
-      case $1 in
-      --cache)
-        _Setup
-        _RcloneCacheMount
-        ;;
-      --mount)
-        _Setup
-        _NormalMount
-        ;;
-      --CachePurge)
-        _purge
-        ;;
-      esac
-      shift
+        case $1 in
+        -p | --purge)
+            _purge
+            ;;
+        -m | --mount)
+            _NormalMount
+            ;;
+        -u | --unmount)
+            _unmount
+            ;;
+        -c | --cache)
+            _RcloneCacheMount
+            ;;
+        -i | --install)
+            _install
+            ;;
+        -h | --help | *)
+            _hlp
+            ;;
+        esac
+        shift
     done
-  }
-
-  ##############################################################################
-
-  _hlp() {
-    $ECHO ""
-    $ECHO "Usage: rclone_mount.sh <option(s)>"
-    $ECHO ""
-    $ECHO "Available options:"
-    $ECHO "-c, --cache    : Mount rclone remote with cacheing parameters"
-    $ECHO "-h, --help     : This help page."
-    $ECHO "-i, --install	: Installs rclone via linuxbrew"
-    $ECHO "-m, --mount    : Mount your rclone remote"
-    $ECHO "-p, --purge    : Will purge your rclone cache and mount."
-    $ECHO ""
-  }
-
-  while [ ! $# -eq 0 ]; do
-    case $1 in
-    -p | --purge)
-      _RcloneMount --CachePurge
-      exit 0
-      ;;
-    -m | --mount)
-      _RcloneMount --mount
-      exit 0
-      ;;
-    -u | --unmount)
-      _unmount
-      exit 0
-      ;;
-    -c | --cache)
-      _RcloneMount --cache
-      exit 0
-      ;;
-    -i | --install)
-      _install
-      exit 0
-      ;;
-    -h | --help | *)
-      _hlp
-      exit 0
-      ;;
-    esac
-    shift
-  done
 }
+
 _Main "$@"
